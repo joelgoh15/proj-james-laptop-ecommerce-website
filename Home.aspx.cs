c8f2f41d1cb4cd7a3bc98b1d40daac378a1887cb
Joel Goh Chong Hui <147377225+joelgoh15@users.noreply.github.com>
2025-02-12 11:36:14 +0800

    upload project files.

---
+++ Home.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Portfolio2_3
+{
+    public partial class Home_Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+
+
+
+
+
+
+    }//end-class
+}//end-namespace
---
+++ PurchaseList.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Portfolio2_3
+{
+    public partial class AdminPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+
+
+
+   
+            }
+        }
+
+
+
+
+    }//end-class
+}//end-namespace
